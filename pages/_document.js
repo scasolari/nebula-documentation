c7ad1ac2e420633941cf0566b7f05dbf29f494c3
sergio.casolari <sergio.casolari@placedv.com>
2023-11-03 10:28:44 +0100

    fix GA4

--- pages/_document.js
+++ pages/_document.js
@@ -7,12 +7,12 @@ export default function Document() {
           <script async src={`https://www.googletagmanager.com/gtag/js?id=G-${process.env.VERCEL_ENV === 'preview' || 'development' ? '4R2ZKTH9XH' : 'D8W7NNPDSZ' }`}></script>
           <script
               dangerouslySetInnerHTML={{
-                  __html: `<script>
+                  __html: `
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-${process.env.VERCEL_ENV === 'preview' || 'development' ? '4R2ZKTH9XH' : 'D8W7NNPDSZ' }');
-</script>`,
+`,
               }}
           />
       </Head>
