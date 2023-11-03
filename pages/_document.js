a00dd64483ed2cd95d130c806a50e4da6a374639
sergio.casolari <sergio.casolari@placedv.com>
2023-11-03 10:02:17 +0100

    added GA4

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=G-${process.env.VERCEL_ENV === 'preview' || 'development' ? '4R2ZKTH9XH' : 'D8W7NNPDSZ' }`}></script>
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-${process.env.VERCEL_ENV === 'preview' || 'development' ? '4R2ZKTH9XH' : 'D8W7NNPDSZ' }');
+</script>`,
+              }}
+          />
+      </Head>
       <body>
         <Main />
         <NextScript />
